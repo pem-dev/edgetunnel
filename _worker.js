baff838f2b3a15b093f3be88c1f2d859dafd7761
pem-dev <62530978+pem-dev@users.noreply.github.com>
2025-09-18 13:42:10 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,7 +1,7 @@
 ﻿
 import { connect } from 'cloudflare:sockets';
 
-let userID = '';
+let userID = '69dd5607-64ec-49d5-b455-67d6d622f387';
 let proxyIP = '';
 let DNS64Server = '';
 //let sub = '';
@@ -7175,4 +7175,5 @@ function config_Html(token = "test", proxyhost = "") {
 </html>`;
 
     return html;
+
 }
